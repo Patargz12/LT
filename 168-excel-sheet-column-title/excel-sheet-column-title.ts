9ac8cb5fea4e0325d2af0d42a1b0be554d0d08b6
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-07-03 15:12:34 +0800

    Time: 0 ms (100.00%) | Memory: 55.6 MB (13.50%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.ts
@@ -0,0 +1,11 @@
+const convertToTitle = (columnNumber) => {
+   let result = "";
+   
+   while (columnNumber > 0) {
+       columnNumber--;
+       result = String.fromCharCode(65 + (columnNumber % 26)) + result;
+       columnNumber = Math.floor(columnNumber / 26);
+   }
+   
+   return result;
+};
