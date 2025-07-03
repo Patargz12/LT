c5141ca13eb377f65c78ea706a720d313f916344
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-07-03 14:22:28 +0800

    Time: 58 ms (67.54%) | Memory: 63.8 MB (69.90%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.ts
@@ -0,0 +1,13 @@
+const getIntersectionNode = (headA, headB) => {
+    if (!headA || !headB) return null;
+    
+    let pA = headA;
+    let pB = headB;
+    
+    while (pA !== pB) {
+        pA = pA ? pA.next : headB;
+        pB = pB ? pB.next : headA;
+    }
+    
+    return pA;
+};
