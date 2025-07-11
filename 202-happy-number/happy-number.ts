4f8e8fd125d266f5c2d2f4d062c59950bc22db21
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-07-11 10:26:13 +0800

    Time: 1 ms (82.59%) | Memory: 57.6 MB (55.43%) - LeetSync

---
+++ 202-happy-number/happy-number.ts
@@ -0,0 +1,20 @@
+const isHappy = n => {
+    const seen = new Set();
+    
+    while (n !== 1 && !seen.has(n)) {
+        seen.add(n);
+        n = getNext(n);
+    }
+    
+    return n === 1;
+};
+
+const getNext = n => {
+    let sum = 0;
+    while (n > 0) {
+        const digit = n % 10;
+        sum += digit * digit;
+        n = Math.floor(n / 10);
+    }
+    return sum;
+};
