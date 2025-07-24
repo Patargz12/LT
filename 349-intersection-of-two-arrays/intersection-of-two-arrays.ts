e1fa08a16353a49d3780f6c5fea0038e58b66b50
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-07-24 16:31:59 +0800

    Time: 1 ms (79.10%) | Memory: 58 MB (5.97%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -0,0 +1,12 @@
+const intersection = (nums1, nums2) => {
+    const set1 = new Set(nums1);
+    const result = new Set();
+    
+    for (const num of nums2) {
+        if (set1.has(num)) {
+            result.add(num);
+        }
+    }
+    
+    return [...result];
+};
