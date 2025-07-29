c9fef015d49b4d569d1023e72ab842e7cc370175
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-07-29 15:49:38 +0800

    Time: 42 ms (83.94%) | Memory: 58.6 MB (87.95%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.ts
@@ -0,0 +1,17 @@
+const canPartition = (nums) => {
+    const sum = nums.reduce((acc, num) => acc + num, 0);
+    
+    if (sum % 2 !== 0) return false;
+    
+    const target = sum / 2;
+    const dp = new Array(target + 1).fill(false);
+    dp[0] = true;
+    
+    for (const num of nums) {
+        for (let j = target; j >= num; j--) {
+            dp[j] = dp[j] || dp[j - num];
+        }
+    }
+    
+    return dp[target];
+};
