9dcba3c994d0f332da79b7dda48889e6be5b2d08
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-01 09:21:07 +0800

    Time: 26 ms (63.86%) | Memory: 62.1 MB (93.68%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.ts
@@ -0,0 +1,16 @@
+function findContentChildren(g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    
+    let i = 0, j = 0, count = 0;
+    
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
+            count++;
+            i++;
+        }
+        j++;
+    }
+    
+    return count;
+}
