cee8ea43c44bb6c4d9921b19dbd3c1859867e31f
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-13 15:57:58 +0800

    Time: 5 ms (92.22%) | Memory: 58.1 MB (39.32%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.ts
@@ -0,0 +1,38 @@
+const lengthOfLIS = (nums) => {
+    const tails = [];
+    
+    for (const num of nums) {
+        let left = 0, right = tails.length;
+        
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2);
+            if (tails[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        if (left === tails.length) {
+            tails.push(num);
+        } else {
+            tails[left] = num;
+        }
+    }
+    
+    return tails.length;
+};
+
+const lengthOfLISDP = (nums) => {
+    const dp = Array(nums.length).fill(1);
+    
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    
+    return Math.max(...dp);
+};
