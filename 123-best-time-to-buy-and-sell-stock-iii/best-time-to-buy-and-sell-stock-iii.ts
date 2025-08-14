65da20fad084e4d23f2fb970ca2220555f6c4bf0
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-14 21:20:25 +0800

    Time: 6 ms (79.63%) | Memory: 66 MB (93.21%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.ts
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.ts
@@ -1,17 +1,17 @@
-function maxProfit(prices) {
+const maxProfit = (prices: number[]): number => {
+    if (prices.length <= 1) return 0;
+    
     let buy1 = -prices[0];
     let sell1 = 0;
     let buy2 = -prices[0];
     let sell2 = 0;
     
     for (let i = 1; i < prices.length; i++) {
-        const price = prices[i];
-        
-        buy1 = Math.max(buy1, -price);
-        sell1 = Math.max(sell1, buy1 + price);
-        buy2 = Math.max(buy2, sell1 - price);
-        sell2 = Math.max(sell2, buy2 + price);
+        buy1 = Math.max(buy1, -prices[i]);
+        sell1 = Math.max(sell1, buy1 + prices[i]);
+        buy2 = Math.max(buy2, sell1 - prices[i]);
+        sell2 = Math.max(sell2, buy2 + prices[i]);
     }
     
     return sell2;
-}
+};
