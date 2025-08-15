7afb8596fb873e6441cf19ae8447e7b22ed05a7f
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-15 09:19:17 +0800

    Time: 9 ms (90.85%) | Memory: 58 MB (86.62%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.ts
@@ -0,0 +1,12 @@
+const change = (amount, coins) => {
+    const dp = Array(amount + 1).fill(0);
+    dp[0] = 1;
+    
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+    
+    return dp[amount];
+};
