8ba7aaf217213c7051155bdb365b4d4b733febb3
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-15 09:24:50 +0800

    Time: 0 ms (100.00%) | Memory: 55.8 MB (11.98%) - LeetSync

--- 198-house-robber/house-robber.ts
+++ 198-house-robber/house-robber.ts
@@ -1,11 +1,12 @@
-var rob = function(nums) {
-   let prev1 = 0, prev2 = 0;
-   
-   for (let num of nums) {
-       let temp = prev1;
-       prev1 = Math.max(prev2 + num, prev1);
-       prev2 = temp;
-   }
-   
-   return prev1;
+const rob = (nums) => {
+    let prev2 = 0;
+    let prev1 = 0;
+    
+    for (const num of nums) {
+        const current = Math.max(prev1, prev2 + num);
+        prev2 = prev1;
+        prev1 = current;
+    }
+    
+    return prev1;
 };
