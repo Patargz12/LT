1a65072ee2086f11ec955b9cf365cdc0ac793941
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-18 13:44:50 +0800

    Time: 4 ms (95.00%) | Memory: 58.2 MB (92.50%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.ts
@@ -0,0 +1,34 @@
+const closedIsland = (grid) => {
+    const m = grid.length;
+    const n = grid[0].length;
+    
+    const dfs = (i, j) => {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === 1) return;
+        
+        grid[i][j] = 1;
+        dfs(i - 1, j);
+        dfs(i + 1, j);
+        dfs(i, j - 1);
+        dfs(i, j + 1);
+    };
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if ((i === 0 || i === m - 1 || j === 0 || j === n - 1) && grid[i][j] === 0) {
+                dfs(i, j);
+            }
+        }
+    }
+    
+    let count = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 0) {
+                count++;
+                dfs(i, j);
+            }
+        }
+    }
+    
+    return count;
+};
