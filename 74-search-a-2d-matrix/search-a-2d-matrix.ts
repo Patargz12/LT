9ed801acf1b6ea566ecae74fee16e38fbb1adcc0
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-24 16:48:48 +0800

    Time: 0 ms (100.00%) | Memory: 55.3 MB (61.02%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -0,0 +1,23 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let left = 0;
+    let right = m * n - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor(left + (right - left) / 2);
+        const row = Math.floor(mid / n);
+        const col = mid % n;
+        const midValue = matrix[row][col];
+        
+        if (midValue === target) {
+            return true;
+        } else if (midValue < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return false;
+}
