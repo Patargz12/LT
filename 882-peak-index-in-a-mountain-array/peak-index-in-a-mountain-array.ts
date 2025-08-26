0d2298901601430f7a5ee565e1886b218520214b
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-26 16:31:07 +0800

    Time: 0 ms (100.00%) | Memory: 65.8 MB (17.52%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.ts
@@ -0,0 +1,16 @@
+function peakIndexInMountainArray(arr: number[]): number {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] < arr[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    
+    return left;
+}
