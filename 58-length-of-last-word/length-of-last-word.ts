d08493080d6e831a32871eb59fd55970f94b1cc0
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-27 23:44:29 +0800

    Time: 0 ms (100.00%) | Memory: 55.7 MB (26.48%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.ts
@@ -0,0 +1,15 @@
+function lengthOfLastWord(s: string): number {
+    let i = s.length - 1;
+    
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+    
+    let length = 0;
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+    
+    return length;
+}
