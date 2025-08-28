eb042165f5e0e83b8c1525be93e1aae1c795fdf2
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-28 11:15:03 +0800

    Time: 0 ms (100.00%) | Memory: 56.4 MB (54.24%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.ts
@@ -0,0 +1,15 @@
+function diagonalSum(mat: number[][]): number {
+    const n = mat.length;
+    let sum = 0;
+    
+    for (let i = 0; i < n; i++) {
+        sum += mat[i][i];
+        sum += mat[i][n - 1 - i];
+    }
+    
+    if (n % 2 === 1) {
+        sum -= mat[Math.floor(n / 2)][Math.floor(n / 2)];
+    }
+    
+    return sum;
+}
