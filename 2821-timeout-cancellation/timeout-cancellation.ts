3c9c348b3f4a9a0be037ee9e85ce27e368ac5cde
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-08-31 14:12:24 +0800

    Time: 58 ms (46.32%) | Memory: 56.1 MB (17.10%) - LeetSync

---
+++ 2821-timeout-cancellation/timeout-cancellation.ts
@@ -0,0 +1,9 @@
+function cancellable(fn: Function, args: any[], t: number): Function {
+   const timeoutId = setTimeout(() => {
+       fn(...args);
+   }, t);
+   
+   return () => {
+       clearTimeout(timeoutId);
+   };
+}
