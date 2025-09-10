2c3c6a49bf433d5771b8c500d442bda2574cea06
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-10 16:10:44 +0800

    Time: 10 ms (87.50%) | Memory: 63.4 MB (72.83%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.ts
@@ -0,0 +1,15 @@
+function findMaxLength(nums: number[]): number {
+    const map = new Map<number, number>()
+    map.set(0, -1)
+    let maxLen = 0
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 1 ? 1 : -1
+        if (map.has(count)) {
+            maxLen = Math.max(maxLen, i - map.get(count)!)
+        } else {
+            map.set(count, i)
+        }
+    }
+    return maxLen
+}
