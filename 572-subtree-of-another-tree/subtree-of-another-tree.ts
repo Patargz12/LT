0ad21a49e1b227ce38eb9a257e8ae53e8265c4c9
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-15 13:11:47 +0800

    Time: 5 ms (85.00%) | Memory: 61.1 MB (98.86%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.ts
@@ -0,0 +1,11 @@
+const isSubtree = (root, subRoot) => {
+    if (!root) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+const isSameTree = (p, q) => {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
