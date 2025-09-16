bbfd4927776ac92f762be3aa9723279defc1cfb1
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-16 20:51:44 +0800

    Time: 457 ms (70.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,11 @@
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+    SELECT MAX(cnt)
+    FROM (
+        SELECT COUNT(order_number) AS cnt
+        FROM Orders
+        GROUP BY customer_number
+    ) AS t
+);
