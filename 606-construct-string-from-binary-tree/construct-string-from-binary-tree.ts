8efade43d591c63d8c96ac98093f40eb1bb1aea6
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-18 16:40:35 +0800

    Time: 0 ms (100.00%) | Memory: 61 MB (36.67%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.ts
@@ -0,0 +1,16 @@
+
+function tree2str(root: TreeNode | null): string {
+    if (!root) return "";
+    
+    let result = root.val.toString();
+    
+    if (root.left || root.right) {
+        result += "(" + tree2str(root.left) + ")";
+    }
+    
+    if (root.right) {
+        result += "(" + tree2str(root.right) + ")";
+    }
+    
+    return result;
+}
