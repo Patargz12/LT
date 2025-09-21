8e6946a89a34181c49a99e71a86cb7b69f22f053
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-21 16:27:47 +0800

    Time: 3 ms (85.29%) | Memory: 73.9 MB (79.41%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.ts
@@ -0,0 +1,13 @@
+function maxDistance(arrays: number[][]): number {
+    let res = 0
+    let minVal = arrays[0][0]
+    let maxVal = arrays[0][arrays[0].length - 1]
+    for (let i = 1; i < arrays.length; i++) {
+        const curMin = arrays[i][0]
+        const curMax = arrays[i][arrays[i].length - 1]
+        res = Math.max(res, Math.abs(curMax - minVal), Math.abs(maxVal - curMin))
+        minVal = Math.min(minVal, curMin)
+        maxVal = Math.max(maxVal, curMax)
+    }
+    return res
+}
