76e4cfab2b5a8116172ae2b10b50abc5ab334561
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-22 13:46:34 +0800

    Time: 2 ms (82.75%) | Memory: 70.2 MB (16.40%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
@@ -0,0 +1,16 @@
+function findMaxAverage(nums: number[], k: number): number {
+    let sum = 0;
+    
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    
+    let maxSum = sum;
+    
+    for (let i = k; i < nums.length; i++) {
+        sum = sum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, sum);
+    }
+    
+    return maxSum / k;
+}
