8c8e52dffbbc58499977790dde4fef2c052bf13d
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-09-22 13:34:57 +0800

    Time: 4 ms (53.67%) | Memory: 61.5 MB (42.20%) - LeetSync

---
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.ts
@@ -0,0 +1,23 @@
+function averageOfLevels(root: TreeNode | null): number[] {
+    if (!root) return [];
+    
+    const result: number[] = [];
+    const queue: TreeNode[] = [root];
+    
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        let levelSum = 0;
+        
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift()!;
+            levelSum += node.val;
+            
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        
+        result.push(levelSum / levelSize);
+    }
+    
+    return result;
+}
