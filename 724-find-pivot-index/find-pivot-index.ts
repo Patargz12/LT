32e70104c2699547887ab9894bc78a17663d43a5
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-06 23:46:44 +0800

    Time: 3 ms (44.57%) | Memory: 58 MB (82.24%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.ts
@@ -0,0 +1,9 @@
+function pivotIndex(nums: number[]): number {
+    let total = nums.reduce((a, b) => a + b, 0)
+    let left = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (left === total - left - nums[i]) return i
+        left += nums[i]
+    }
+    return -1
+}
