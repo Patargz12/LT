c5a79b364ee579e846b071bbc4f3af89edee657b
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-17 21:47:16 +0800

    Time: 4 ms (77.86%) | Memory: 61.1 MB (81.68%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.ts
@@ -0,0 +1,15 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  if (k <= 1) return 0
+  let left = 0
+  let prod = 1
+  let count = 0
+  for (let right = 0; right < nums.length; right++) {
+    prod *= nums[right]
+    while (prod >= k) {
+      prod /= nums[left]
+      left++
+    }
+    count += right - left + 1
+  }
+  return count
+}
