9aa657f66da6487e9e319cb3f2854f6769bd6d50
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-20 15:20:59 +0800

    Time: 3 ms (86.67%) | Memory: 63.2 MB (11.11%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.ts
+++ 445-add-two-numbers-ii/add-two-numbers-ii.ts
@@ -1,27 +1,31 @@
-const addTwoNumbers = (l1, l2) => {
-    const stack1 = [];
-    const stack2 = [];
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const stack1: number[] = [];
+  const stack2: number[] = [];
+  
+  while (l1) {
+    stack1.push(l1.val);
+    l1 = l1.next;
+  }
+  
+  while (l2) {
+    stack2.push(l2.val);
+    l2 = l2.next;
+  }
+  
+  let carry = 0;
+  let result: ListNode | null = null;
+  
+  while (stack1.length || stack2.length || carry) {
+    const val1 = stack1.pop() ?? 0;
+    const val2 = stack2.pop() ?? 0;
+    const sum = val1 + val2 + carry;
     
-    while (l1) {
-        stack1.push(l1.val);
-        l1 = l1.next;
-    }
-    
-    while (l2) {
-        stack2.push(l2.val);
-        l2 = l2.next;
-    }
-    
-    let carry = 0;
-    let result = null;
-    
-    while (stack1.length || stack2.length || carry) {
-        const sum = (stack1.pop() || 0) + (stack2.pop() || 0) + carry;
-        carry = Math.floor(sum / 10);
-        
-        const newNode = { val: sum % 10, next: result };
-        result = newNode;
-    }
-    
-    return result;
-};
+    carry = Math.floor(sum / 10);
+    const node = new ListNode(sum % 10);
+    node.next = result;
+    result = node;
+  }
+  
+  return result;
+}
