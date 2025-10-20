144e5f92bb21d10e53a82fef34054eddcfed6127
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-20 15:44:33 +0800

    Time: 2 ms (61.29%) | Memory: 56.9 MB (58.06%) - LeetSync

---
+++ 256-paint-house/paint-house.ts
@@ -0,0 +1,18 @@
+function minCost(costs: number[][]): number {
+  if (costs.length === 0) return 0;
+  
+  const n = costs.length;
+  const dp = Array.from({ length: n }, () => Array(3).fill(0));
+  
+  dp[0][0] = costs[0][0];
+  dp[0][1] = costs[0][1];
+  dp[0][2] = costs[0][2];
+  
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+    dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+    dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+  }
+  
+  return Math.min(dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]);
+}
