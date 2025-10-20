284318b478144ecd3e724af2cac8139b4e1fbf39
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-20 15:17:40 +0800

    Time: 60 ms (8.57%) | Memory: 62.6 MB (28.57%) - LeetSync

---
+++ 2740-curry/curry.ts
@@ -0,0 +1,8 @@
+function curry(fn: Function): Function {
+  return function curried(...args: any[]): any {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+    return (...nextArgs: any[]) => curried(...args, ...nextArgs);
+  };
+}
