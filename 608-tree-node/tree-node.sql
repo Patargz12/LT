fbd70cddae42276308ac11e7da9be99f85b2687e
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-10-28 21:50:49 +0800

    Time: 408 ms (92.19%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,9 +1,8 @@
 SELECT 
     id,
-    CASE 
+    CASE
         WHEN p_id IS NULL THEN 'Root'
-        WHEN id IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        WHEN id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
         ELSE 'Leaf'
     END AS type
-FROM Tree
-ORDER BY id;
+FROM Tree;
