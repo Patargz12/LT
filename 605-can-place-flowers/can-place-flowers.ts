46849f6d45e19dcf5429a68c01859664fa262258
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-11-15 23:31:41 +0800

    Time: 1 ms (83.24%) | Memory: 58.7 MB (15.30%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.ts
+++ 605-can-place-flowers/can-place-flowers.ts
@@ -2,12 +2,18 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
     let count = 0;
     
     for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 0 && 
-            (i === 0 || flowerbed[i - 1] === 0) && 
-            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
-            flowerbed[i] = 1;
-            count++;
-            if (count >= n) return true;
+        if (flowerbed[i] === 0) {
+            const leftEmpty = i === 0 || flowerbed[i - 1] === 0;
+            const rightEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+            
+            if (leftEmpty && rightEmpty) {
+                flowerbed[i] = 1;
+                count++;
+                
+                if (count >= n) {
+                    return true;
+                }
+            }
         }
     }
     
