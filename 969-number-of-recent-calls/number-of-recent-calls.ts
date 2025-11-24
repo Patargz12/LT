8bebee77d81b237a932aaafc8f728b7e593ec223
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-11-24 22:05:39 +0800

    Time: 32 ms (84.79%) | Memory: 70.2 MB (79.30%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.ts
@@ -0,0 +1,15 @@
+class RecentCounter {
+  q: number[]
+
+  constructor() {
+    this.q = []
+  }
+
+  ping(t: number): number {
+    this.q.push(t)
+
+    while (this.q[0] < t - 3000) this.q.shift()
+
+    return this.q.length
+  }
+}
