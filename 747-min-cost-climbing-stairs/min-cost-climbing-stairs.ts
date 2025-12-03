3bed3ed35a9053a70e63d4deb5d8a8e004fa40fa
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-03 23:58:52 +0800

    Time: 0 ms (100.00%) | Memory: 56.2 MB (76.96%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.ts
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.ts
@@ -1,12 +1,12 @@
-const minCostClimbingStairs = (cost) => {
+function minCostClimbingStairs(cost: number[]): number {
     let prev2 = 0;
     let prev1 = 0;
     
-    for (let i = 0; i < cost.length; i++) {
-        const current = cost[i] + Math.min(prev1, prev2);
+    for (let i = 2; i <= cost.length; i++) {
+        const current = Math.min(prev1 + cost[i - 1], prev2 + cost[i - 2]);
         prev2 = prev1;
         prev1 = current;
     }
     
-    return Math.min(prev1, prev2);
-};
+    return prev1;
+}
