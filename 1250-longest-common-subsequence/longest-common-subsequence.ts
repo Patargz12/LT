c9bfe48be9f7c8dddb89e37458507b2c23653eea
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-04 00:01:25 +0800

    Time: 20 ms (98.25%) | Memory: 56.8 MB (95.10%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -0,0 +1,20 @@
+function longestCommonSubsequence(text1: string, text2: string): number {
+    const m = text1.length;
+    const n = text2.length;
+    const dp: number[] = new Array(n + 1).fill(0);
+    
+    for (let i = 1; i <= m; i++) {
+        let prev = 0;
+        for (let j = 1; j <= n; j++) {
+            const temp = dp[j];
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[j] = prev + 1;
+            } else {
+                dp[j] = Math.max(dp[j], dp[j - 1]);
+            }
+            prev = temp;
+        }
+    }
+    
+    return dp[n];
+}
