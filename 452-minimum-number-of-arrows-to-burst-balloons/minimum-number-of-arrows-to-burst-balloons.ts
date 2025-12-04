6be5488e8fd757f0912a51c68ae1889c939ff1b3
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-04 21:33:29 +0800

    Time: 79 ms (88.13%) | Memory: 79.5 MB (63.01%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.ts
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.ts
@@ -1,13 +1,13 @@
-function findMinArrowShots(points) {
+function findMinArrowShots(points: number[][]): number {
     points.sort((a, b) => a[1] - b[1]);
     
     let arrows = 1;
-    let arrowPos = points[0][1];
+    let end = points[0][1];
     
     for (let i = 1; i < points.length; i++) {
-        if (points[i][0] > arrowPos) {
+        if (points[i][0] > end) {
             arrows++;
-            arrowPos = points[i][1];
+            end = points[i][1];
         }
     }
     
