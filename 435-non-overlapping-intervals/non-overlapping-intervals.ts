f3849978b55822d1df3c89c5750ada85021442de
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-04 21:32:53 +0800

    Time: 73 ms (70.82%) | Memory: 86.7 MB (67.26%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.ts
+++ 435-non-overlapping-intervals/non-overlapping-intervals.ts
@@ -1,16 +1,16 @@
-const eraseOverlapIntervals = intervals => {
+function eraseOverlapIntervals(intervals: number[][]): number {
     intervals.sort((a, b) => a[1] - b[1]);
     
     let count = 0;
-    let lastEnd = intervals[0][1];
+    let end = intervals[0][1];
     
     for (let i = 1; i < intervals.length; i++) {
-        if (intervals[i][0] < lastEnd) {
+        if (intervals[i][0] < end) {
             count++;
         } else {
-            lastEnd = intervals[i][1];
+            end = intervals[i][1];
         }
     }
     
     return count;
-};
+}
