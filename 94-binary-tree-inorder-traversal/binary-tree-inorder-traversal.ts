030ec8ecd6cbe956baaf4eed6514bf3c78f893f5
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-17 14:51:24 +0800

    Time: 0 ms (100.00%) | Memory: 54.6 MB (77.79%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -1,14 +1,14 @@
 function inorderTraversal(root: TreeNode | null): number[] {
-  const result: number[] = [];
-  
-  function traverse(node: TreeNode | null): void {
-    if (!node) return;
+    const result: number[] = [];
     
-    traverse(node.left);
-    result.push(node.val);
-    traverse(node.right);
-  }
-  
-  traverse(root);
-  return result;
+    function traverse(node: TreeNode | null): void {
+        if (!node) return;
+        
+        traverse(node.left);
+        result.push(node.val);
+        traverse(node.right);
+    }
+    
+    traverse(root);
+    return result;
 }
