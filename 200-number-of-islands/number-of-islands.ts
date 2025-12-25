dae159d4ba6c95f216d8f44320692e758c6a4394
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-25 23:26:07 +0800

    Time: 69 ms (49.24%) | Memory: 59.3 MB (74.32%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.ts
@@ -0,0 +1,26 @@
+function numIslands(grid: string[][]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+    let count = 0;
+    
+    const dfs = (i: number, j: number): void => {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === '0') return;
+        
+        grid[i][j] = '0';
+        dfs(i + 1, j);
+        dfs(i - 1, j);
+        dfs(i, j + 1);
+        dfs(i, j - 1);
+    };
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                dfs(i, j);
+            }
+        }
+    }
+    
+    return count;
+}
