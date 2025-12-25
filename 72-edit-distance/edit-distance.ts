0925868646979dd5cd57b3946ab8264a6648d02c
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-25 23:16:49 +0800

    Time: 9 ms (65.65%) | Memory: 62.4 MB (22.17%) - LeetSync

--- 72-edit-distance/edit-distance.ts
+++ 72-edit-distance/edit-distance.ts
@@ -1,26 +1,25 @@
 function minDistance(word1: string, word2: string): number {
     const m = word1.length;
     const n = word2.length;
-    const dp: number[] = new Array(n + 1);
     
-    for (let j = 0; j <= n; j++) {
-        dp[j] = j;
-    }
+    const dp: number[][] = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
+    
+    for (let i = 0; i <= m; i++) dp[i][0] = i;
+    for (let j = 0; j <= n; j++) dp[0][j] = j;
     
     for (let i = 1; i <= m; i++) {
-        let prev = dp[0];
-        dp[0] = i;
-        
         for (let j = 1; j <= n; j++) {
-            const temp = dp[j];
             if (word1[i - 1] === word2[j - 1]) {
-                dp[j] = prev;
+                dp[i][j] = dp[i - 1][j - 1];
             } else {
-                dp[j] = Math.min(prev, dp[j], dp[j - 1]) + 1;
+                dp[i][j] = Math.min(
+                    dp[i - 1][j] + 1,
+                    dp[i][j - 1] + 1,
+                    dp[i - 1][j - 1] + 1
+                );
             }
-            prev = temp;
         }
     }
     
-    return dp[n];
+    return dp[m][n];
 }
