f031bb04d49a1d6c29270b91c6afb2cae83ed21c
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-25 23:19:23 +0800

    Time: 9 ms (32.47%) | Memory: 58.9 MB (21.04%) - LeetSync

--- 139-word-break/word-break.ts
+++ 139-word-break/word-break.ts
@@ -1,11 +1,11 @@
-const wordBreak = (s, wordDict) => {
+function wordBreak(s: string, wordDict: string[]): boolean {
     const wordSet = new Set(wordDict);
-    const dp = Array(s.length + 1).fill(false);
+    const dp: boolean[] = new Array(s.length + 1).fill(false);
     dp[0] = true;
     
     for (let i = 1; i <= s.length; i++) {
         for (let j = 0; j < i; j++) {
-            if (dp[j] && wordSet.has(s.substring(j, i))) {
+            if (dp[j] && wordSet.has(s.slice(j, i))) {
                 dp[i] = true;
                 break;
             }
@@ -13,4 +13,4 @@ const wordBreak = (s, wordDict) => {
     }
     
     return dp[s.length];
-};
+}
