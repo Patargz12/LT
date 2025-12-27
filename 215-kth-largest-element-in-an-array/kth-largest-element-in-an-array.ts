a01cf70821b4c0bec64aaac849515fffbfe49009
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-27 11:45:50 +0800

    Time: 17 ms (89.68%) | Memory: 70.3 MB (43.21%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.ts
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.ts
@@ -1,38 +1,43 @@
 function findKthLargest(nums: number[], k: number): number {
-  const heap: number[] = []
-  function push(x: number) {
-    heap.push(x)
-    let i = heap.length - 1
-    while (i > 0) {
-      const p = (i - 1) >> 1
-      if (heap[p] <= heap[i]) break
-      ;[heap[p], heap[i]] = [heap[i], heap[p]]
-      i = p
+    const partition = (left: number, right: number): [number, number] => {
+        const pivotIndex = left + Math.floor(Math.random() * (right - left + 1));
+        const pivot = nums[pivotIndex];
+        [nums[pivotIndex], nums[right]] = [nums[right], nums[pivotIndex]];
+        
+        let lt = left;
+        let gt = right;
+        let i = left;
+        
+        while (i <= gt) {
+            if (nums[i] > pivot) {
+                [nums[lt], nums[i]] = [nums[i], nums[lt]];
+                lt++;
+                i++;
+            } else if (nums[i] < pivot) {
+                [nums[i], nums[gt]] = [nums[gt], nums[i]];
+                gt--;
+            } else {
+                i++;
+            }
+        }
+        
+        return [lt, gt];
+    };
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const [lt, gt] = partition(left, right);
+        
+        if (k - 1 < lt) {
+            right = lt - 1;
+        } else if (k - 1 > gt) {
+            left = gt + 1;
+        } else {
+            return nums[k - 1];
+        }
     }
-  }
-  function pop() {
-    const top = heap[0]
-    const last = heap.pop()!
-    if (heap.length) {
-      heap[0] = last
-      let i = 0
-      const n = heap.length
-      while (true) {
-        let l = i * 2 + 1
-        let r = i * 2 + 2
-        let smallest = i
-        if (l < n && heap[l] < heap[smallest]) smallest = l
-        if (r < n && heap[r] < heap[smallest]) smallest = r
-        if (smallest === i) break
-        ;[heap[i], heap[smallest]] = [heap[smallest], heap[i]]
-        i = smallest
-      }
-    }
-    return top
-  }
-  for (const x of nums) {
-    push(x)
-    if (heap.length > k) pop()
-  }
-  return heap[0]
+    
+    return nums[k - 1];
 }
