bada7cbb1b043b62f6903fa2e833724b623d6667
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-28 23:30:25 +0800

    Time: 0 ms (100.00%) | Memory: 55.2 MB (48.22%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.ts
+++ 54-spiral-matrix/spiral-matrix.ts
@@ -1,32 +1,33 @@
 function spiralOrder(matrix: number[][]): number[] {
     const result: number[] = [];
-    const m = matrix.length;
-    const n = matrix[0].length;
+    if (!matrix.length) return result;
     
-    let top = 0, bottom = m - 1;
-    let left = 0, right = n - 1;
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
     
     while (top <= bottom && left <= right) {
-        for (let col = left; col <= right; col++) {
-            result.push(matrix[top][col]);
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
         }
         top++;
         
-        for (let row = top; row <= bottom; row++) {
-            result.push(matrix[row][right]);
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
         }
         right--;
         
         if (top <= bottom) {
-            for (let col = right; col >= left; col--) {
-                result.push(matrix[bottom][col]);
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
             }
             bottom--;
         }
         
         if (left <= right) {
-            for (let row = bottom; row >= top; row--) {
-                result.push(matrix[row][left]);
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
             }
             left++;
         }
