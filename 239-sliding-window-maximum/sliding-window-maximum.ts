64a405b5fe9743a5b1328af2ba3e237305227a62
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-29 19:38:28 +0800

    Time: 425 ms (48.48%) | Memory: 86.7 MB (46.21%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.ts
+++ 239-sliding-window-maximum/sliding-window-maximum.ts
@@ -1,13 +1,13 @@
-const maxSlidingWindow = (nums, k) => {
-    const deque = [];
-    const result = [];
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    const result: number[] = [];
+    const deque: number[] = [];
     
     for (let i = 0; i < nums.length; i++) {
-        while (deque.length > 0 && deque[0] < i - k + 1) {
+        while (deque.length && deque[0] < i - k + 1) {
             deque.shift();
         }
         
-        while (deque.length > 0 && nums[deque[deque.length - 1]] <= nums[i]) {
+        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
             deque.pop();
         }
         
@@ -19,18 +19,4 @@ const maxSlidingWindow = (nums, k) => {
     }
     
     return result;
-};
-
-const maxSlidingWindowBruteForce = (nums, k) => {
-    const result = [];
-    
-    for (let i = 0; i <= nums.length - k; i++) {
-        let max = nums[i];
-        for (let j = i; j < i + k; j++) {
-            max = Math.max(max, nums[j]);
-        }
-        result.push(max);
-    }
-    
-    return result;
-};
+}
