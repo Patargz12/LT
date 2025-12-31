60a51d79bfcf2be42f93ed7c325d9361700080ec
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-31 23:43:04 +0800

    Time: 0 ms (100.00%) | Memory: 54.6 MB (74.86%) - LeetSync

--- 394-decode-string/decode-string.ts
+++ 394-decode-string/decode-string.ts
@@ -1,25 +1,24 @@
 function decodeString(s: string): string {
     const stack: string[] = [];
-    const countStack: number[] = [];
-    let currentStr = '';
-    let k = 0;
     
     for (const char of s) {
-        if (char >= '0' && char <= '9') {
-            k = k * 10 + parseInt(char);
-        } else if (char === '[') {
-            stack.push(currentStr);
-            countStack.push(k);
-            currentStr = '';
-            k = 0;
-        } else if (char === ']') {
-            const repeatCount = countStack.pop()!;
-            const prevStr = stack.pop()!;
-            currentStr = prevStr + currentStr.repeat(repeatCount);
+        if (char !== ']') {
+            stack.push(char);
         } else {
-            currentStr += char;
+            let str = '';
+            while (stack[stack.length - 1] !== '[') {
+                str = stack.pop() + str;
+            }
+            stack.pop();
+            
+            let num = '';
+            while (stack.length && !isNaN(Number(stack[stack.length - 1]))) {
+                num = stack.pop() + num;
+            }
+            
+            stack.push(str.repeat(Number(num)));
         }
     }
     
-    return currentStr;
+    return stack.join('');
 }
