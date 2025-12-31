d33618aadfc1a9e6c7cae53a54f55f1fdcc58d1f
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2025-12-31 23:42:31 +0800

    Time: 9 ms (62.66%) | Memory: 66.2 MB (53.76%) - LeetSync

--- 155-min-stack/min-stack.ts
+++ 155-min-stack/min-stack.ts
@@ -1,30 +1,22 @@
 class MinStack {
-    private stack: number[];
-    private minStack: number[];
-    
-    constructor() {
-        this.stack = [];
-        this.minStack = [];
-    }
-    
+    private stack: number[] = [];
+    private minStack: number[] = [];
+
     push(val: number): void {
         this.stack.push(val);
-        if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
-            this.minStack.push(val);
-        }
+        const currentMin = this.minStack.length === 0 ? val : Math.min(val, this.minStack[this.minStack.length - 1]);
+        this.minStack.push(currentMin);
     }
-    
+
     pop(): void {
-        const val = this.stack.pop();
-        if (val === this.minStack[this.minStack.length - 1]) {
-            this.minStack.pop();
-        }
+        this.stack.pop();
+        this.minStack.pop();
     }
-    
+
     top(): number {
         return this.stack[this.stack.length - 1];
     }
-    
+
     getMin(): number {
         return this.minStack[this.minStack.length - 1];
     }
