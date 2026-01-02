0e1e9bd0d0a9f5008fd679b6fa30a2b8cd5a5cb2
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-01-02 16:11:55 +0800

    Time: 1 ms (98.48%) | Memory: 63.3 MB (79.33%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.ts
@@ -0,0 +1,19 @@
+function firstMissingPositive(nums: number[]): number {
+    const n = nums.length;
+    
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            const temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+    
+    return n + 1;
+}
