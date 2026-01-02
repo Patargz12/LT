3239f04a1a3d71f42d0a5a11b76cd1829627a1ad
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-01-02 16:16:13 +0800

    Time: 4 ms (48.98%) | Memory: 68.1 MB (22.33%) - LeetSync

---
+++ 189-rotate-array/rotate-array.ts
@@ -0,0 +1,15 @@
+function rotate(nums: number[], k: number): void {
+    k = k % nums.length;
+    
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+}
+
+function reverse(nums: number[], left: number, right: number): void {
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+}
