15d4a35f324f2fe94db331a24e33dd6193e5ea65
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-01-02 16:10:49 +0800

    Time: 44 ms (63.66%) | Memory: 79.7 MB (48.26%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.ts
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.ts
@@ -1,56 +1,42 @@
-class TrieNode {
-    children: Map<string, TrieNode>;
-    isEndOfWord: boolean;
+class Trie {
+    children: Map<string, Trie>;
+    isEnd: boolean;
     
     constructor() {
         this.children = new Map();
-        this.isEndOfWord = false;
+        this.isEnd = false;
     }
-}
 
-class Trie {
-    private root: TrieNode;
-    
-    constructor() {
-        this.root = new TrieNode();
-    }
-    
     insert(word: string): void {
-        let node = this.root;
-        
+        let node: Trie = this;
         for (const char of word) {
             if (!node.children.has(char)) {
-                node.children.set(char, new TrieNode());
+                node.children.set(char, new Trie());
             }
             node = node.children.get(char)!;
         }
-        
-        node.isEndOfWord = true;
+        node.isEnd = true;
     }
-    
+
     search(word: string): boolean {
-        let node = this.root;
-        
+        let node: Trie = this;
         for (const char of word) {
             if (!node.children.has(char)) {
                 return false;
             }
             node = node.children.get(char)!;
         }
-        
-        return node.isEndOfWord;
+        return node.isEnd;
     }
-    
+
     startsWith(prefix: string): boolean {
-        let node = this.root;
-        
+        let node: Trie = this;
         for (const char of prefix) {
             if (!node.children.has(char)) {
                 return false;
             }
             node = node.children.get(char)!;
         }
-        
         return true;
     }
 }
