c5cfac803a8985fc35c3c0538b7a7126a64856f2
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-01-11 19:18:28 +0800

    Time: 1 ms (78.95%) | Memory: 56.9 MB (42.10%) - LeetSync

---
+++ 928-surface-area-of-3d-shapes/surface-area-of-3d-shapes.ts
@@ -0,0 +1,17 @@
+function surfaceArea(grid: number[][]): number {
+    const n = grid.length;
+    let area = 0;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] > 0) {
+                area += 2 + 4 * grid[i][j];
+                
+                if (i > 0) area -= 2 * Math.min(grid[i][j], grid[i - 1][j]);
+                if (j > 0) area -= 2 * Math.min(grid[i][j], grid[i][j - 1]);
+            }
+        }
+    }
+    
+    return area;
+}
