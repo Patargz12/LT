e387ec34ade07848b46709881177b78807af84bc
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-01-29 23:07:06 +0800

    Time: 12 ms (66.13%) | Memory: 65.6 MB (22.58%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(s: string): string {
+    const stack: string[] = [];
+    
+    for (const char of s) {
+        if (stack.length && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.join('');
+}
