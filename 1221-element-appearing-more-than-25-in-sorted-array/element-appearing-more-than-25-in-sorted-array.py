796132e6c61a2b5e634df1bca6066ee8fccc9c97
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-02-12 02:58:45 +0800

    Time: 3 ms (47.99%) | Memory: 20.6 MB (49.03%) - LeetSync

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        threshold = len(arr) // 4
+        for i in range(len(arr)):
+            if arr[i] == arr[i + threshold]:
+                return arr[i]
