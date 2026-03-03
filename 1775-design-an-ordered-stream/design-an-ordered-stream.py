f8093779e72cc77aabbb5cf8d1ee73bafbc09c2d
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2026-03-03 21:53:07 +0800

    Time: 24 ms (15.54%) | Memory: 20 MB (62.28%) - LeetSync

---
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -0,0 +1,12 @@
+class OrderedStream:
+    def __init__(self, n: int):
+        self.stream = [None] * (n + 1)
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> list[str]:
+        self.stream[idKey] = value
+        result = []
+        while self.ptr < len(self.stream) and self.stream[self.ptr]:
+            result.append(self.stream[self.ptr])
+            self.ptr += 1
+        return result
